3d1e4c282ef5713b62c6ecae83990fde2038e12a
Nikita Shavrov <na206851@gmail.com>
2025-07-29 22:36:50 +0800

    абстрактный класс для определения общих методов геометрических фигур

---
+++ src/main/lesson00/ParamOfGeometricShapes.java
@@ -0,0 +1,14 @@
+package lesson00;
+
+public abstract class ParamOfGeometricShapes {
+    public double height;
+
+    public abstract double baseArea();
+
+    public abstract double crossSectionalAreaFlatness(int heightFlatness);
+
+    public abstract double surfaceSquare();
+
+    public abstract double volumeFigure();
+}
+
