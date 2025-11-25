c7549fb7917d710aeb924cc493f472eb1537f40e
Nikita Shavrov <na206851@gmail.com>
2025-11-25 22:38:45 +1200

    валидация количества продукта нужного клиенту

--- src/test/testLesson00/testProductClass/ValidateTestProduct/validateNameProduct.java
+++ src/test/testLesson00/testProductClass/ValidateTestProduct/validateNameProduct.java
@@ -4,7 +4,7 @@ import lesson00.ProductMain.ProductMainClass;
 import testLesson00.testProductClass.ValidateInterface.validate;
 
 public class validateNameProduct implements validate {
-
+    
     @Override
     public boolean Check(ProductMainClass product) {
         return product != null && product.toString().trim().isEmpty();
