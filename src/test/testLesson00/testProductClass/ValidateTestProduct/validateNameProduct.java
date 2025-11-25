3bf51c42e138a920ee12f3bb187ed361989b740d
Nikita Shavrov <na206851@gmail.com>
2025-11-25 22:17:31 +1200

    валидациция имени продукта

---
+++ src/test/testLesson00/testProductClass/ValidateTestProduct/validateNameProduct.java
@@ -0,0 +1,12 @@
+package testLesson00.testProductClass.ValidateTestProduct;
+
+import lesson00.ProductMain.ProductMainClass;
+import testLesson00.testProductClass.ValidateInterface.validate;
+
+public class validateNameProduct implements validate {
+
+    @Override
+    public boolean Check(ProductMainClass product) {
+        return product != null && product.toString().trim().isEmpty();
+    }
+}
