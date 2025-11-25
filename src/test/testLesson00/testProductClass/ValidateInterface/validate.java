1e6868d431b2d7227ef4f0fe5914a71b2da791df
Nikita Shavrov <na206851@gmail.com>
2025-11-25 22:16:46 +1200

    интерфейс для проверки класса по отдельным полям класса ProductMainClass

---
+++ src/test/testLesson00/testProductClass/ValidateInterface/validate.java
@@ -0,0 +1,7 @@
+package testLesson00.testProductClass.ValidateInterface;
+
+import lesson00.ProductMain.ProductMainClass;
+
+public interface validate {
+    boolean Check(ProductMainClass product);
+}
