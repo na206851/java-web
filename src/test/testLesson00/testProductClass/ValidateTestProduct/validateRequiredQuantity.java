b07629e9683a1d6bc231ef012f579d784d83aea0
Nikita Shavrov <na206851@gmail.com>
2025-11-25 22:18:25 +1200

    валидация количества продукта нужного клиенту

---
+++ src/test/testLesson00/testProductClass/ValidateTestProduct/validateRequiredQuantity.java
@@ -0,0 +1,15 @@
+package testLesson00.testProductClass.ValidateTestProduct;
+
+import lesson00.ProductMain.ProductMainClass;
+import testLesson00.testProductClass.ValidateInterface.validate;
+
+public class validateRequiredQuantity implements validate {
+
+    @Override
+    public boolean Check(ProductMainClass product) {
+        if (product.requiredQuantity <= 0) {
+            return false;
+        }
+        return product.requiredQuantity <= product.quantityInStock;
+    }
+}
