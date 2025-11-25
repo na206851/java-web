9be9b898b895faff0f818531a512a62b2ea58a09
Nikita Shavrov <na206851@gmail.com>
2025-11-25 22:15:42 +1200

    main class for validate product

---
+++ src/main/lesson00/ProductMain/ProductMainClass.java
@@ -0,0 +1,33 @@
+package lesson00.ProductMain;
+
+import java.util.Date;
+
+public class ProductMainClass {
+
+    public String nameProduct;
+    public int requiredQuantity;   //требуемое количество
+    public int quantityInStock;    //количество на складе
+    public int productPrice;       //цена продукта
+    public int sizeDiscount;       //размер скидки в рублях
+    public int IDClient;       //индивидуальный номер клиента
+    public String emailClient;     //электронная почта клиента
+    public int phoneNumberClient;      //телефонный номер клиента
+    public Date dateDelivery;
+
+    public ProductMainClass() {
+    }
+
+    public ProductMainClass(String nameProduct, int requiredQuantity, int quantityInStock,
+                            int productPrice, int sizeDiscount, int IDClient, String emailClient, int phoneNumberClient, Date dateDelivery) {
+        this.nameProduct = nameProduct;
+        this.requiredQuantity = requiredQuantity;
+        this.quantityInStock = quantityInStock;
+        this.productPrice = productPrice;
+        this.sizeDiscount = sizeDiscount;
+        this.IDClient = IDClient;
+        this.emailClient = emailClient;
+        this.phoneNumberClient = phoneNumberClient;
+        this.dateDelivery = dateDelivery;
+
+    }
+}
