6f1201ca5a7d32bf057fb1d425e2a1ea4cb6e922
Nikita Shavrov <na206851@gmail.com>
2025-11-27 16:31:20 +1200

    базовая проверка на положительный размер скидку на продукт

--- src/test/testLesson00/testProductClass/ValidateTestProduct/validateSaleDiscount.java
+++ src/test/testLesson00/testProductClass/ValidateTestProduct/validateSaleDiscount.java
@@ -1,5 +1,15 @@
 package testLesson00.testProductClass.ValidateTestProduct;
 
-public class validateSaleDiscount {
+import lesson00.ProductMain.ProductMainClass;
+import testLesson00.testProductClass.ValidateInterface.validate;
 
+public class validateSaleDiscount implements validate {
+
+    @Override
+    public boolean Check(ProductMainClass product) {
+        if (product.sizeDiscount < 0) {
+            return false;
+        }
+        
+    }
 }
