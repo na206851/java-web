98805071143217f22ee876a4013da09752ad30d3
Nikita Shavrov <na206851@gmail.com>
2025-12-09 19:37:51 +1200

    добавил методы get/set для полей класса

--- src/main/lesson00/ProductMain/ProductMainClass.java
+++ src/main/lesson00/ProductMain/ProductMainClass.java
@@ -1,7 +1,5 @@
 package lesson00.ProductMain;
 
-import java.util.Date;
-
 public class ProductMainClass {
 
     public String nameProduct;
@@ -12,7 +10,92 @@ public class ProductMainClass {
     public String IDClient;       //индивидуальный номер клиента
     public String emailClient;     //электронная почта клиента
     public String phoneNumberClient;      //телефонный номер клиента
-//    public Date dateDelivery;       //дата доставки клиенту
+    public String dateDelivery;       //дата доставки клиенту
+
+    public ProductMainClass(String nameProduct, int requiredQuantity, int quantityInStock,
+                            int productPrice, int sizeDiscount, String IDClient, String emailClient, String phoneNumberClient, String dateDelivery) {
+        this.nameProduct = nameProduct; //название продукта
+        this.requiredQuantity = requiredQuantity;   //требуемое количество
+        this.quantityInStock = quantityInStock;     //количество на складе
+        this.productPrice = productPrice;           //цена товара
+        this.sizeDiscount = sizeDiscount;           //размер скидке
+        this.IDClient = IDClient;                   //имя клиента
+        this.emailClient = emailClient;             //электронная почта клиента
+        this.phoneNumberClient = phoneNumberClient; //номер телефона клиента
+        this.dateDelivery = dateDelivery;           //дата доставки клиенту
+    }
+
+    public void setNameProduct(String nameProduct) {
+        this.nameProduct = nameProduct;
+    }
+
+    public void setRequiredQuantity(int requiredQuantity) {
+        this.requiredQuantity = requiredQuantity;
+    }
+
+    public void setQuantityInStock(int quantityInStock) {
+        this.quantityInStock = quantityInStock;
+    }
+
+    public void setProductPrice(int productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public void setSizeDiscount(int sizeDiscount) {
+        this.sizeDiscount = sizeDiscount;
+    }
+
+    public void setIDClient(String IDClient) {
+        this.IDClient = IDClient;
+    }
+
+    public void setEmailClient(String emailClient) {
+        this.emailClient = emailClient;
+    }
+
+    public void setPhoneNumberClient(String phoneNumberClient) {
+        this.phoneNumberClient = phoneNumberClient;
+    }
+
+    public void setDateDelivery(String dateDelivery) {
+        this.dateDelivery = dateDelivery;
+    }
+
+    public String getNameProduct() {
+        return nameProduct;
+    }
+
+    public int getRequiredQuantity() {
+        return requiredQuantity;
+    }
+
+    public int getQuantityInStock() {
+        return quantityInStock;
+    }
+
+    public int getProductPrice() {
+        return productPrice;
+    }
+
+    public int getSizeDiscount() {
+        return sizeDiscount;
+    }
+
+    public String getIDClient() {
+        return IDClient;
+    }
+
+    public String getEmailClient() {
+        return emailClient;
+    }
+
+    public String getPhoneNumberClient() {
+        return phoneNumberClient;
+    }
+
+    public String getDateDelivery() {
+        return dateDelivery;
+    }
 
     public ProductMainClass() {
     }
